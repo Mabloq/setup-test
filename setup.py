3c781b929849349f79f0299b6aa7f493e6f9330b
Matthew Arcila <arcilamatt@gmail.com>
2019-07-05 22:24:09 -0400

    fixt setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 setup(
     name='setup_test',
     version='1.0',
-    packages=find_packages,
+    packages=find_packages(),
     install_requires=requirements,
     url='https://github.com/Mabloq/setup-test.git',
     license='MIT',
